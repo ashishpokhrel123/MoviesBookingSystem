dfa2cc7e8c10b5006c59c537d352215b2c38f9cb
sadinajmagar1999@gmail.com <aashishpokhrel146@gmail.com>
2019-04-14 23:31:13 +0545

    final document updated

--- Implemantation/Movies_Booking/tests/Unit/UserTest.php
+++ Implemantation/Movies_Booking/tests/Unit/UserTest.php
@@ -16,19 +16,90 @@ class LoginTest extends TestCase
      */
     public function testLogin()
     {
-        $email="aashishpokhrel146@gmail.com";
-        $password="Aashish123";
-        $response=$this->call("GET", "/login/$email/$password");
-        $this->assertEquals(404, $response->status());
+        $email="anishbudhathoki123@gmail.com";
+        $password="Anish123";
+        $response=$this->call("GET", '/login',[
+         'username'=>$email,
+         'password'=>$password
+
+         ]);
+         $this->assertEquals(200, $response->status());
     }
-//    public function testRegister()
-//    {
-//        $name="Ashish";
-//        $address="ktm";
-//        $phone="981235677";
-//        $email="aashishpokhrel125@gmail.com";
-//        $password="Ashish123";
-//        $response=$this->call("POST","/register/$name/$address/$phone/$email/$password");
-//        $this->assertEquals(404,$response->status());
-//    }
+
+    public function testRegister()
+ {
+      $name="Anish";
+       $address="ktm";
+      $phone="981235677";
+      $email="anishbudhathoki123@gmail.com";
+     $password="Anish123";
+      $response=$this->call("POST",'/register',[
+          'name'=>$name,
+          'address'=>$address,
+          'phone'=>$phone,
+          'username'=>$email,
+       'password'=>$password
+      ]);
+     $this->assertEquals(302,$response->status());
+  }
+  public function testSendMessage()
+    {
+     $mov_title="tottal dhamal";
+     $mov_director="anil sharma";
+     $mov_cast="anil kappor";
+     $mov_type="comedy";
+     $mov_lang="hindi";
+     $mov_realasedate="2019-04-05";
+     $mov_duration="120min";
+     $mov_url="qwerty";
+     $image="image";
+     $mov_description="this is fanstic movie";
+     $mov_description="1";
+         $response=$this->call("POST",'/addMovie',[
+             'mov_title'=>$mov_title,
+             'mov_director'=> $mov_director,
+             'mov_cast'=>$mov_cast,
+             'mov_type'=> $mov_type,
+             'mov_lang'=> $mov_lang,
+             'mov_realasedate'=> $mov_realasedate,
+             'mov_duration'=>  $mov_duration,
+             'mov_url'=>   $mov_url,
+             'image'=> $image,
+             'mov_description'=> $mov_description,
+             'realasedstatus'=> $mov_description,
+         ]);
+         $this->assertEquals(302,$response->status());
+     }
+     public function testFrienRequest()
+       {
+           $show_date="2019-04-08";
+           $show_time="12:00:30 Am";
+           $movie="total dhamal";
+           $response=$this->call("POST",'/addShow',[
+               'show_date'=>$show_date,
+               'show_time'=>$show_time,
+               'movies'=> $movie,
+
+
+           ]);
+          $this->assertEquals(302,$response->status());
+       }
+       public function testSearchuser()
+  {
+      $book_id="1";
+      $mov_id="1";
+      $show_id="1";
+      $screen="audi1";
+      $book_seats="1,2,3";
+      $totprice="Rs 900";
+      $response=$this->call("POST",'/chooseseat',[
+          'book_id'=>$book_id,
+          '$mov_id'=>$mov_id,
+          '$show_id'=> $show_id,
+           'screen'=>$screen,
+           'book_seats'=>$book_seats,
+           'totprice'=>$totprice,
+      ]);
+     $this->assertEquals(302,$response->status());
+   }
 }
